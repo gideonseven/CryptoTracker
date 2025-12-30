240ad7f1fde45977fc501156362fb5f699139675
Gideon Tobing <gideon.tobing@gmail.com>
2025-12-30 19:32:28 +1100

    finish up list ui

---
+++ app/src/main/java/com/gt/cryptotracker/crypto/presentation/coin_list/components/CoinListState.kt
@@ -0,0 +1,11 @@
+package com.gt.cryptotracker.crypto.presentation.coin_list.components
+
+import androidx.compose.runtime.Immutable
+import com.gt.cryptotracker.crypto.presentation.models.CoinUi
+
+@Immutable
+data class CoinListState(
+    val isLoading: Boolean = false,
+    val coins: List<CoinUi> = emptyList(),
+    val selectedCoin: CoinUi? = null
+)
