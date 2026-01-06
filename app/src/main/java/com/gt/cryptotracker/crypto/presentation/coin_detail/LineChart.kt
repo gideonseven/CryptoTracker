57498d499ddfe30364085c533f8e73737a37e6cc
gideon tobing <gideon.tobing@gmail.com>
2026-01-07 00:19:17 +1000

    add line viewport height

--- app/src/main/java/com/gt/cryptotracker/crypto/presentation/coin_detail/LineChart.kt
+++ app/src/main/java/com/gt/cryptotracker/crypto/presentation/coin_detail/LineChart.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun LineChart(
@@ -71,5 +72,22 @@ fun LineChart(
         val verticalPaddingPx = style.verticalPadding.roundToPx()
         val horizontalPaddingPx = style.horizontalPadding.roundToPx()
         val xAxisLabelSpacingPx = style.xAxisLabelSpacing.roundToPx()
+
+        //measure the text
+        val xLabelTextLayoutResults = visibleDataPoints.map {
+            measurer.measure(
+                text = it.xLabel,
+                style = textStyle.copy(textAlign = TextAlign.Center)
+            )
+        }
+
+        val maxXLabelWidth = xLabelTextLayoutResults.maxOfOrNull { it.size.width } ?: 0
+        val maxXLabelHeight = xLabelTextLayoutResults.maxOfOrNull { it.size.height } ?: 0
+        val maxXLabelLineCount = xLabelTextLayoutResults.maxOfOrNull { it.lineCount } ?: 0
+        val xLabelLineHeight = maxXLabelHeight / maxXLabelLineCount
+
+
+        val viewPortHeightPx =
+            size.height - (maxXLabelHeight + 2 * verticalPaddingPx + xLabelLineHeight * xAxisLabelSpacingPx)
     }
 }
